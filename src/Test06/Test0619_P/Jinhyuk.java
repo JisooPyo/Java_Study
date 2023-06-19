93143ddd6656b0847035897d64243b5b440df3cf
jin <132440453+05030522@users.noreply.github.com>
2023-06-19 10:52:34 +0900

    jinhyuk

--- src/Test06/Test0619_P/Jinhyuk.java
+++ src/Test06/Test0619_P/Jinhyuk.java
@@ -1,43 +1,44 @@
 // lv.2 JadenCase 문자열 만들기
 package Test06.Test0619_P;
 
-import java.util.ArrayList;
-
 public class Jinhyuk {
     class Solution {
         public String solution(String s) {
+            // StringBuilder은 모든유형의 데이터를 받아들인다.
+            // StringBuilder 객체 생성
+            StringBuilder answer = new StringBuilder();
 
-            // asnwer 선언 및 초기화
-            String answer = "";
+            // space 선언 및 초기화
+            boolean space = true;
 
-            // 문자열을 띄어쓰기로 분리(공백문자 연속해서 나올 수 없음)
-            String[] splitWord = s.split("\\s");
+            // for반복문을 사용
+            for (int i = 0; i < s.length(); i++) {
 
-            // result 라는 arraylist 리스트 생성
-            ArrayList<String> result = new ArrayList<>();
-
-            // 글들을 나눔
-            for (int i = 0; i < splitWord.length; i++) {
-                // 첫번째 글자를 나눠서 firstWord
-                String firstWord = splitWord[i].substring(0, 1);
-                // 두번째 글자를 나눠서 restWord
-                String restWord = splitWord[i].substring(1);
-
-                // 첫 글자를 대문자로 변환
-                firstWord = firstWord.toUpperCase();
-
-                // 나머지를 소문자로 변환
-                restWord = restWord.toLowerCase();
-
-                // result 에 대소문자로 변환한 결과값을 합쳐서 담음
-                result.add(firstWord + restWord);
+                // s의 각 문자들은 cut으로 저장
+                char cut = s.charAt(i);
+                // cut 이 띄어쓰기인지 아닌지 확인
+                if (cut == ' ') {
+                    //space 를 트루로 설정
+                    space = true;
+                    // result 속에 cut 문자들을 넣는다.
+                    answer.append(cut);
+                    // cut 가 띄어쓰기가 아니라면
+                } else {
+                    // space 가 트루일 경우
+                    if (space) {
+                        // answer 에다가 cut 들을 대문자로 변환하여 추가
+                        answer.append(Character.toUpperCase(cut));
+                        // space 값을 false로 설정
+                        space = false;
+                    } else {
+                        // space 가 false 인 경우
+                        // answer 에 cut 을 소문자로 변환하여 추가
+                        answer.append(Character.toLowerCase(cut));
+                    }
+                }
             }
-            // answer 에 result 리스트 속 값을 join 통해서 띄워쓰기로 구분해서 하나의 문자열로 만듬
-            answer = String.join(" ", result);
-
-            // 정답을 return
-            return answer;
-
+            // answer 속 값들을 문자열로 출력
+            return answer.toString();
         }
     }
 }
