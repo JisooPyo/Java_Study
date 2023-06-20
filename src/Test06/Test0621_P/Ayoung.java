09d6569535fe2330d8f39b4e07c55ac98fbff4c0
ayoung <ayoung3052@gmail.com>
2023-06-20 12:32:53 +0900

    Ayoung_P_약수의 개수와 덧셈
    약수 구하기 (함수) - 1부터 자기 자신까지의 수로 나누어 몫이 0이면 count +1을 한다
    
    덧셈, 뺄셈 하기 - 위 함수의 리턴값이 짝수면 덧셈, 홀수면 뺄셈을 한다

--- src/Test06/Test0621_P/Ayoung.java
+++ src/Test06/Test0621_P/Ayoung.java
@@ -2,4 +2,37 @@
 package Test06.Test0621_P;
 
 public class Ayoung {
+    class Solution {
+        public int solution(int left, int right) {
+            int answer = 0; // 정답
+            for (int i = left; i <= right; i++){ // 왼쪽 ~ 오른쪽 값
+                if(countDivisor(i) %2 == 0) { // 짝수면 덧셈
+                    answer+= i;
+                } else{ // 홀수면 뺄셈
+                    answer -= i;
+                }
+            }
+            System.out.println(answer);
+            return answer;
+        }
+
+        // 약수의 갯수를 구하는 함수
+        public int countDivisor (int input){
+            int count = 0; // 약수의 갯수를 세는 변수
+            for(int i=1; i <= input; i++){
+                if(input % i == 0) count ++; // 약수가 있을 경우 count ++
+            }
+            System.out.print(count + ">>" ); // 결과 찍어보기
+            return count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        solution.solution(14,17);
+        solution.solution(1,27);
+        solution.solution(35,45);
+
+    }
 }
