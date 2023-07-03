57ba71c69ba35405b3e11f809cc2a929494dd559
Haena <johanna0123@naver.com>
2023-07-03 12:18:26 +0900

    Haena_P_3진법 뒤집기 두 번째 풀이
    * 풀이 과정
    1. 주어진 10진수의 수를 toString을 통해 3진수로 바꾼 후 StringBuiler에 담는다.(뒤집기 위함)
    2. 3진수를 뒤집은 문자열을 parseInt로 바꿔 10진수를 구한다.
    
    * 테스트 통과 시간: 0.04ms ~ 0.07ms

--- src/Test07/Test0703_P/Haena2.java
+++ src/Test07/Test0703_P/Haena2.java
@@ -10,28 +10,20 @@ public class Haena2 {
 
 class SolutionHn2 {
 	public int solution(int n) {
-		StringBuilder sb = new StringBuilder();
-
-		// 뒤집어진 3진수 구하기
-		while(n != 0){
-			sb.append(n % 3);
-			n /= 3;
-		}
-
-		// parseInt를 활용하여 10진수 구하기
-		return Integer.parseInt(sb.toString(),3);
+		StringBuilder sb = new StringBuilder(Integer.toString(n,3));
+		return Integer.parseInt(sb.reverse().toString(),3);
 	}
 }
 
 /*
-테스트 1 〉	통과 (0.03ms, 73MB)
-테스트 2 〉	통과 (0.05ms, 70.8MB)
-테스트 3 〉	통과 (0.04ms, 66.5MB)
-테스트 4 〉	통과 (0.05ms, 76.2MB)
-테스트 5 〉	통과 (0.04ms, 71.6MB)
-테스트 6 〉	통과 (0.05ms, 79.2MB)
-테스트 7 〉	통과 (0.04ms, 72.2MB)
-테스트 8 〉	통과 (0.05ms, 73.3MB)
-테스트 9 〉	통과 (0.04ms, 76.7MB)
-테스트 10 〉	통과 (0.06ms, 75.3MB)
+테스트 1 〉	통과 (0.06ms, 77.9MB)
+테스트 2 〉	통과 (0.07ms, 76.1MB)
+테스트 3 〉	통과 (0.04ms, 75.3MB)
+테스트 4 〉	통과 (0.06ms, 73MB)
+테스트 5 〉	통과 (0.05ms, 77.5MB)
+테스트 6 〉	통과 (0.05ms, 73.1MB)
+테스트 7 〉	통과 (0.04ms, 71.3MB)
+테스트 8 〉	통과 (0.06ms, 87.4MB)
+테스트 9 〉	통과 (0.05ms, 72.4MB)
+테스트 10 〉	통과 (0.04ms, 75.4MB)
  */
